5a0691a4061ab82a9b9fa6d860f7f5ac1e6a30a8
Declan Naughton <declann1990@gmail.com>
2024-04-16 15:40:08 +0100

    build

--- dist/_npm/isarray@1.0.0/_esm.js
+++
@@ -1,7 +0,0 @@
-/**
- * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
- * Original file: /npm/isarray@1.0.0/index.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-var r={}.toString,a=Array.isArray||function(a){return"[object Array]"==r.call(a)};export{a as default};
